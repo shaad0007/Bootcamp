cedf2ac05d5695476cd1817f8ef1a90c9d647002
shaad0007 <123370160+shaad0007@users.noreply.github.com>
2023-08-13 17:04:05 +0530

    new file added

---
+++ practice/script.js
@@ -0,0 +1,52 @@
+// document.write("Hello world" + "<br>");
+// document.write("<b><i>Hello world</b></i>");
+
+// document.write("<b><i>Hello world</b></i>");
+/*document.write("<b><i>Hello world</b></i>");
+document.write("<b><i>Hello world</b></i>");*/
+
+// var jj, ans;
+
+// jj = 100;
+// ans = 500;
+
+// console.log(ans);
+// console.log(jj);
+
+// var a = 10;
+// document.write(a);
+
+// let a = "73";
+// let b = "hello";
+// let c = 58;
+// let d = true;
+// let e;
+// let f = ["hello", 46];
+// let g = { name: "shaad", age: 36 };
+// let h = null;
+// document.write(typeof a + "<br>");
+
+// document.write(typeof b + "<br>");
+// document.write(typeof c + "<br>");
+// document.write(typeof d + "<br>");
+// document.write(typeof e + "<br>");
+// document.write(typeof f + "<br>");
+// document.write(typeof g + "<br>");
+// document.write(typeof h + "<br>");
+
+// console.time();
+
+// console.log("shaad");
+// console.error("i am danger");
+// console.warn("terminator");
+// console.table([1, 2, 3, 4, 5]);
+// console.timeEnd();
+// console.clear();
+
+let a = 0;
+let b = 10;
+if (!a > b) {
+  console.log("yes");
+} else {
+  console.log("No");
+}
